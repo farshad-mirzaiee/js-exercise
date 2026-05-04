ba70783d49c4d332722d5f8b00236628a06f62f6
Ali Farshad Mirzaiee <118207714+farshad-mirzaiee@users.noreply.github.com>
2026-05-04 22:45:50 +0500

    Added functions to JS.

--- exercise_2/script.js
+++ exercise_2/script.js
@@ -1,3 +1,8 @@
 const select = document.querySelector("select");
 const list = document.querySelector("ul");
 const h1 = document.querySelector("h1");
+
+select.addEventListener("change", () => {
+    const choice = select.value;
+    creatCalender(choice);
+});
