83920589203720dff4de2773fad327d77c8d6d82
Ali Farshad Mirzaiee <118207714+farshad-mirzaiee@users.noreply.github.com>
2026-05-05 11:53:04 +0500

    finished the calender exercise.

--- exercise_2/script.js
+++ exercise_2/script.js
@@ -4,5 +4,31 @@ const h1 = document.querySelector("h1");
 
 select.addEventListener("change", () => {
     const choice = select.value;
-    creatCalender(choice);
+    createCalender(choice);
 });
+
+function createCalender(month) {
+    let days = 31;
+
+    if (month === "February") {
+        days = 28;
+    } else if (
+        month === "April" ||
+        month === "June" ||
+        month === "September" ||
+        month === "November"
+    ) {
+        days = 30;
+    }
+
+    list.textContent = "";
+    h1.textContent = month;
+    for (let i = 1; i <= days; i++) {
+        const listItem = document.createElement("li");
+        listItem.textContent = i;
+        list.appendChild(listItem);
+    }
+}
+
+select.value = "January";
+createCalender("January");
