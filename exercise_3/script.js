654746e3e1d8586e51107fd5694563f89a5bd870
Ali Farshad Mirzaiee <118207714+farshad-mirzaiee@users.noreply.github.com>
2026-05-11 17:43:57 +0500

    Added the script to the javascript file.

--- exercise_3/script.js
+++ exercise_3/script.js
@@ -1,2 +1,14 @@
 const select = document.querySelector('select');
 const para = document.querySelector('p');
+
+select.addEventListener("change", setWeather);
+
+function setWeather() {
+    const choice = select.value;
+
+    if(choice === 'sunny') {
+        para.textContent = 'It is nice and sunny today. Wear shorts! Go to the beach, or to the park, and get an ice cream.';
+    } else if (choice = 'rainy') {
+        para.textContent = 'It is rainy outside; take a rain coat and a brolly, and don\'t stay out for too long.'
+    }
+}
