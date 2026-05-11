41fe5f7cea174ca76c0b605d1c95d92d2c259cfa
Ali Farshad Mirzaiee <118207714+farshad-mirzaiee@users.noreply.github.com>
2026-05-11 17:49:51 +0500

    Finished the exercise.

--- exercise_3/script.js
+++ exercise_3/script.js
@@ -8,7 +8,13 @@ function setWeather() {
 
     if(choice === 'sunny') {
         para.textContent = 'It is nice and sunny today. Wear shorts! Go to the beach, or to the park, and get an ice cream.';
-    } else if (choice = 'rainy') {
-        para.textContent = 'It is rainy outside; take a rain coat and a brolly, and don\'t stay out for too long.'
+    } else if (choice === 'rainy') {
+        para.textContent = 'It is rainy outside; take a rain coat and a brolly, and don\'t stay out for too long.';
+    } else if (choice === 'snowy') {
+        para.textContent = 'Winter is coming! Best to go to the wall and join the Night Watchers! Become the Lord commander';
+    } else if (choice === 'overcast') {
+        para.textContent = 'It is cloudy! Not raining yet, but best to take a rain coat and a brolly just be on the safe side.';
+    } else {
+        para.textContent = '';
     }
 }
